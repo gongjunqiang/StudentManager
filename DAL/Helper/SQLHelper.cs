8a2c6a130d0a84713568917c49e427b63e8e11ee
龚均强 <1208754526@qq.com>
2019-07-25 23:20:07 +0800

    111

---
+++ DAL/Helper/SQLHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    /// <summary>
+    /// 通用数据访问类
+    /// </summary>
+    public class SQLHelper
+    {
+        public static readonly string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
+
+        /// <summary>
+        /// 执行update、insert、delete等操作
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="sqlParameter"></param>
+        /// <returns></returns>
+        public static int Update(string sql, SqlParameter[] sqlParameter=null)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            if (sqlParameter != null)
+            {
+                cmd.Parameters.AddRange(sqlParameter);
+            }
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("方法public static int Update执行异常,异常信息："+ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        /// <summary>
+        /// 执行单一结果集
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static object GetSingalResulrt (string sql)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("方法public static object GetSingalResulrt执行异常,异常信息：" + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        /// <summary>
+        /// 执行结果集查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static SqlDataReader GetReader(string sql)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                throw new Exception("方法public static SqlDataReader GetReader：" + ex.Message);
+            }
+        }
+    }
+}
